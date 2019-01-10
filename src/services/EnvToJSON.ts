00abdc65bbe263587922556d5f0b10423db0ffc9
Mustafa İlhan <ilhan.mstf@gmail.com>
2019-01-10 18:18:23 +0300

    merge variables of .env with config.json

---
+++ src/services/EnvToJSON.ts
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv"
+
+export default function (): object  {
+  function converToNumberIfItIsNumber (val: string): any {
+    return /^\d*$/.test(val) ? Number(val) : val
+  }
+
+  let obj: any = {}
+  let envVars: any = dotenv.config()
+  if ('parsed' in envVars) {
+    envVars = envVars['parsed']
+    let keys: string[] = Object.keys(envVars)
+
+    for (let i: number = 0, ii: number = keys.length; i !== ii; i++) {
+      envVars[keys[i]] = converToNumberIfItIsNumber(envVars[keys[i]])
+
+      let lowerKey: string = keys[i].toLowerCase()
+      let key: string = lowerKey.substring(0, lowerKey.indexOf('_'))
+      let subKey: string = lowerKey.substring(lowerKey.indexOf('_') + 1)
+      obj[key] = obj[key] || {}
+      obj[key][subKey] = envVars[keys[i]]
+    }
+  }
+  return obj
+}
