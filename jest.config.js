5ac2b9e8db625f8a8375f45f93c080273df3b250
begumerden <13107117+begumerden@users.noreply.github.com>
2019-01-16 23:30:17 +0300

    #26 prevent jest duplicate mock warning

--- jest.config.js
+++ jest.config.js
@@ -80,7 +80,7 @@ module.exports = {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: ['<rootDir>/.*/__mocks__'],
 
   // Activates notifications for test results
   // notify: false,
