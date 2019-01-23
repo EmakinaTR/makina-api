b383c4eab05f1f8d24be8a74bf7220da97fe6a9e
Mustafa İlhan <ilhan.mstf@gmail.com>
2019-01-23 17:58:31 +0300

    Adds Date scalar

--- src/gql/Common.ts
+++ src/gql/Common.ts
@@ -1,4 +1,9 @@
+import { GraphQLScalarType } from 'graphql'
+import { Kind } from 'graphql/language'
+
 export const typeDefs = `
+  scalar Date
+
   type DBResponseRaw {
     fieldCount: Int
     affectedRows: Int
@@ -11,4 +16,23 @@ export const typeDefs = `
   }
 `
 export const resolvers = {
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    parseValue (value) {
+      return new Date(value) // value from the client
+    },
+    serialize (value) {
+      return value // value sent to the client
+    },
+    parseLiteral (ast) {
+      // value from the client
+      if (ast.kind === Kind.INT) {
+        return new Date(ast.value)
+      } else if (ast.kind === Kind.STRING) {
+        return new Date(ast.value)
+      }
+      return null
+    }
+  })
 }
